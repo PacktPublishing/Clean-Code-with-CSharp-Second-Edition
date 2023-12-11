619d1768d4367590386c51d6c337bd49601264c4
Jason Alls <jasonalls@hotmail.com>
2023-12-11 02:49:00 +0000

    Added 1st code solution to CH10 as requested by Packt.

---
+++ CH10/CH11/CH11_ThreadDispatching/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CH11_ThreadDispatching
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
